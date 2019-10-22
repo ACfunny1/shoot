cb3eeb3995c6b76cc2552dbd80ac2e75d5127902
sk <997657630@qq.com>
2019-10-23 00:40:07 +0800

    10.23

--- NewProject_3/assets/script/logo.js
+++ NewProject_3/assets/script/logo.js
@@ -16,11 +16,17 @@ cc.Class({
         fireBar: cc.Node,
         bingDu1: cc.Node,
         bingDu2: cc.Node,
+        BDuWeiba1: cc.Node,
+        BDuWeiba2: cc.Node,
+        BDcircle1: cc.Node,
+        BDcircle2: cc.Node,
+        XGT: cc.Node
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        this.XGT.active = false
         var flicker = cc.sequence(
             cc.fadeOut(0.5),
             cc.fadeIn(0.5),
@@ -33,14 +39,19 @@ cc.Class({
     },
     
     start () {
+       cc.log(this.init()) 
+    },
 
+    randomNum (lower,upper) {
+        return Math.floor(Math.random() * (upper - lower));
     },
 
     barAction () {
         var bar = cc.sequence(
             cc.scaleTo(0.4,1,1),
             cc.delayTime(0.1),
-            cc.callFunc(this.bingDuAct,this)
+            cc.callFunc(this.bingDuAct,this),
+            cc.callFunc(this.allFlicker(this.fireBar),this)
             )
         this.fireBar.runAction(bar)
     },
@@ -48,15 +59,72 @@ cc.Class({
     bingDuAct () {
         //1.420,191   2.439,-23.8
         var act = cc.spawn(
-            cc.moveTo(0.3,cc.v2(420,191)),
+            cc.moveTo(0.2,cc.v2(420,191)),
             cc.scaleTo(0.3,1,1)
         )
+        
         var act2 = cc.spawn(
-            cc.moveTo(0.3,cc.v2(439,-23.8)),
-            cc.scaleTo(0.3,1,1)
+            cc.moveTo(0.2,cc.v2(439,-23.8)),
+            cc.scaleTo(0.3,1,1),
         )
-        this.bingDu1.runAction(act)
-        this.bingDu2.runAction(act2)
+
+        var acts = cc.sequence(
+            act,
+            cc.callFunc(this.BDweibaActive,this),
+            cc.spawn(
+                cc.callFunc(this.allFlicker(this.BDcircle1),this),
+                cc.callFunc(this.BDshake(this.bingDu1),this)
+            )
+            
+            //cc.callFunc(this.BDshake(this.bingDu1),this)
+            )
+
+        var act2s = cc.sequence(
+            act2,
+            cc.callFunc(this.BDweibaActive,this),
+            cc.callFunc(this.allFlicker(this.BDcircle2),this),
+            )
+
+        this.bingDu1.runAction(acts)
+        this.bingDu2.runAction(act2s)
+    },
+
+    BDweibaActive () {
+            this.BDuWeiba1.active = false,
+            this.BDuWeiba2.active = false
+    },
+
+    allFlicker (target) {
+        var allFlicker = cc.repeatForever(
+            cc.sequence(
+                cc.fadeOut(0.5),
+                cc.fadeIn(0.5)
+            )
+        )
+
+        target.runAction(allFlicker)
+    },
+
+    BDshake (target) {
+        //x上限275，下限425 y上80，下230
+        var cx = this.randomNum(1,10)
+        var cy = this.randomNum(1,10)
+        var shanke = cc.sequence(
+            cc.moveTo(0.5,cc.v2(275,80)),
+            cc.delayTime(1),
+            cc.log('结束'),
+            cc.moveTo(0.5,cc.v2(425,230)),
+        )
+        target.runAction(shanke)
+    },
+    init () {
+        var cx = this.randomNum(1,10)
+        var cy = this.randomNum(1,10)
+        return 1
     }
-    // update (dt) {},
+
+    
+    /*update (dt) {
+        cc.log(this.BDshake.cx)
+    }*/
 });
