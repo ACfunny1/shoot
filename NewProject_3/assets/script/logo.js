a10da43952da27a3eb7f9f460c3f2ee443fa7cf9
ACfunny1 <997657630@qq.com>
2019-10-24 09:27:21 +0800

    10.24new

--- NewProject_3/assets/script/logo.js
+++ NewProject_3/assets/script/logo.js
@@ -25,77 +25,80 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
         this.XGT.active = false
         var flicker = cc.sequence(
             cc.fadeOut(0.5),
             cc.fadeIn(0.5),
             cc.fadeOut(0.5),
             cc.fadeIn(0.5),
-            cc.callFunc(this.barAction,this),
-            )
+            cc.callFunc(this.barAction, this),
+        )
 
         this.fireFlicker.runAction(flicker)
         cc.log(123)
     },
-    
-    start () {
-       cc.log(this.init()) 
+
+    start() {
+        // cc.log(this.init()) 
     },
 
-    randomNum (lower,upper) {
-        return Math.floor(Math.random()*(upper - lower + 1)) + lower;
+    randomNum(lower, upper) {
+        return Math.floor(Math.random() * (upper - lower + 1)) + lower;
     },
 
-    barAction () {
+    barAction() {
         var bar = cc.sequence(
-            cc.scaleTo(0.4,1,1),
+            cc.scaleTo(0.4, 1, 1),
             cc.delayTime(0.1),
-            cc.callFunc(this.bingDuAct,this),
-            cc.callFunc(this.allFlicker(this.fireBar),this)
-            )
+            cc.callFunc(this.bingDuAct, this),
+            cc.callFunc(this.allFlicker(this.fireBar), this)
+        )
         this.fireBar.runAction(bar)
     },
 
-    bingDuAct () {
+    bingDuAct() {
         //1.420,191   2.439,-23.8
         var act = cc.spawn(
-            cc.moveTo(0.2,cc.v2(420,191)),
-            cc.scaleTo(0.3,1,1)
+            cc.moveTo(0.1, cc.v2(420, 191)),
+            cc.scaleTo(0.2, 1, 1)
         )
-        
+
         var act2 = cc.spawn(
-            cc.moveTo(0.2,cc.v2(439,-23.8)),
-            cc.scaleTo(0.3,1,1),
+            cc.moveTo(0.1, cc.v2(439, -23.8)),
+            cc.scaleTo(0.2, 1, 1),
         )
 
         var acts = cc.sequence(
             act,
-            cc.callFunc(this.BDweibaActive,this),
+            cc.callFunc(this.BDweibaActive, this),
             cc.spawn(
-                cc.callFunc(this.allFlicker(this.BDcircle1),this),
-                cc.callFunc(this.BDshake(this.bingDu1),this)
+                cc.callFunc(this.allFlicker(this.BDcircle1), this),
+                cc.callFunc(this.BDshake(this.bingDu1, 420, 191), this)
             )
 
             //cc.callFunc(this.BDshake(this.bingDu1),this)
-            )
+        )
 
         var act2s = cc.sequence(
             act2,
-            cc.callFunc(this.BDweibaActive,this),
-            cc.callFunc(this.allFlicker(this.BDcircle2),this),
+            cc.callFunc(this.BDweibaActive, this),
+            cc.spawn(
+                cc.callFunc(this.allFlicker(this.BDcircle2), this),
+                cc.callFunc(this.BDshake(this.bingDu2, 439, -23.8), this)
             )
+        )
 
         this.bingDu1.runAction(acts)
         this.bingDu2.runAction(act2s)
     },
 
-    BDweibaActive () {
-            this.BDuWeiba1.active = false,
+    BDweibaActive() {
+        this.BDuWeiba1.active = false,
             this.BDuWeiba2.active = false
     },
 
-    allFlicker (target) {
+    allFlicker(target) {
         var allFlicker = cc.repeatForever(
             cc.sequence(
                 cc.fadeOut(0.5),
@@ -106,26 +109,25 @@ cc.Class({
         target.runAction(allFlicker)
     },
 
-    BDshake (target) {
-        //x上限275，下限425 y上80，下230
-        var cx = this.randomNum(354,500)
-        var cy = this.randomNum(141,260)
-        var a  = target.position
-        var shanke = cc.sequence(
-            cc.moveTo(0.5,cc.v2(cx,cy)),
-            cc.delayTime(1),
-            cc.log('结束'),
-            cc.log(target.position)
-        )
+    BDshake(target, x, y) {
+        //x上限354，下限500 y上141，下260
+        var cx
+        var cy
+        var a = target.position
+        var moveAction = cc.moveBy(0.5, cx, cy);
+        var shanke = cc.repeatForever(cc.sequence(
+            cc.callFunc(function () {
+                cx = this.randomNum(1, 60);
+                cy = this.randomNum(1, 60);
+                moveAction = cc.moveBy(0.5, cx, cy);
+                target.runAction(moveAction)
+            }, this),
+            cc.moveTo(0.5, cc.v2(x, y))
+        ))
         target.runAction(shanke)
     },
-    init () {
-        var cx = this.randomNum(1,10)
-        var cy = this.randomNum(1,10)
-        return 1
-    }
 
-    
+
     /*update (dt) {
         cc.log(this.BDshake.cx)
     }*/
